dea099ca95de749672512c479ea670fe1a34ce22
soloistnxe <quyuxuan0820@163.com>
2020-12-31 12:49:28 +0800

    增加所有

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -27,7 +27,7 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       // 这是登录表单的数据绑定对象
       loginForm: {
@@ -51,11 +51,11 @@ export default {
   },
   methods: {
     // 点击重置按钮，重置登录表单
-    resetLoginForm () {
+    resetLoginForm() {
       // console.log(this);
       this.$refs.loginFormRef.resetFields()
     },
-    login () {
+    login() {
       this.$refs.loginFormRef.validate(async valid => {
         if (!valid) return
         const { data: res } = await this.$http.post('login', this.loginForm)
